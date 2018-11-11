3ac573d34544ff45c2c81deb485417fc4bf4e765
Alan Scarpa <alan.scarpa@gmail.com>
2018-11-10 18:48:32 -0800

    add ui tests

--- Blog Nerd RanchUITests/Blog_Nerd_RanchUITests.swift
+++ Blog Nerd RanchUITests/Blog_Nerd_RanchUITests.swift
@@ -9,27 +9,37 @@
 import XCTest
 
 class Blog_Nerd_RanchUITests: XCTestCase {
-        
+    
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+    func testThatTappingGroupByButtonPresentsAlertController() {
+        let blogPostsNavigationBar = app.navigationBars["Blog Posts"]
+        blogPostsNavigationBar.buttons["Group By"].tap()
+        let sheetsQuery = app.sheets
+        
+        XCTAssert(sheetsQuery.staticTexts["Group the posts by..."].exists)
+    }
+    
+    func testThatTappingSortButtonPresentsAlertController() {
+        let blogPostsNavigationBar = app.navigationBars["Blog Posts"]
+        blogPostsNavigationBar.buttons["Sort"].tap()
+        let sheetsQuery = app.sheets
+        
+        XCTAssert(sheetsQuery.staticTexts["Sort the posts by..."].exists)
+    }
+    
     func testThatTappingOnAMetadataCellShowsThatPost() {
-        let app = XCUIApplication()
         let firstCell = app.collectionViews.children(matching: .cell).element(boundBy: 0)
         let titleText = firstCell.staticTexts.element(matching: .any, identifier: "Post Title").label
         firstCell.tap()
